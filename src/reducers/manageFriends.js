0f6e087a1b86370be6c4c7b3760d89e68bd481a5
yammik <mkim0514@gmail.com>
2018-11-16 12:15:01 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,11 +1,11 @@
 export function manageFriends(state, action) {
   switch (action.type) {
     case "ADD_FRIEND":
-      return Object.assign({}, state, {friends: [...state.friends, action.friend]});
-      // return {...state, friends: [...state.friends, action.friend]};
+      // return Object.assign({}, state, {friends: [...state.friends, action.friend]});
+      return {...state, friends: [...state.friends, action.friend]};
     case "REMOVE_FRIEND":
-      return Object.assign({}, state, {friends: state.friends.filter(friend => friend.id !== action.id)});
-      // return {...state, numberOfPresents: state.numberOfPresents + 1};
+      // return Object.assign({}, state, {friends: state.friends.filter(friend => friend.id !== action.id)});
+      return {...state, friends: state.friends.filter(friend => friend.id !== action.id)};
     default:
       return state;
   }
